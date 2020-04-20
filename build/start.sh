973cb67ed0f83002cdc314fd314aaaee2eb6fd21
edgd1er <edgd1er@hotmail.com>
2020-04-20 07:34:33 +0200

    set tzdata during building and running

--- build/start.sh
+++ build/start.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 
+set -x
+##
+# set zone info
+#
+if [[ $(cat /etc/timezone) != "$TZ" ]]; then
+  ln -snf /usr/share/zoneinfo/$TZ /etc/localtime
+  dpkg-reconfigure -f noninteractive tzdata
+fi
 ##
 # Run a command or start supervisord
 #
+
 if [ $# -gt 0 ];then
     # If we passed a command, run it
     exec "$@"
 else
     # Otherwise start supervisord
-    /usr/bin/supervisord -c /etc/supervisord/supervisord.conf
+    /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
 fi
